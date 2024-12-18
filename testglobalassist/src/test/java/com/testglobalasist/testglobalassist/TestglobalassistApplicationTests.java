99986baa8985cc6d4545a42556ad23d22b089cb1
MircoSantoni <santonimircoariel@gmail.com>
2024-12-18 20:57:26 -0300

    Config
    Config on Aplicattion properties with .env and pom config.

---
+++ testglobalassist/src/test/java/com/testglobalasist/testglobalassist/TestglobalassistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testglobalasist.testglobalassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestglobalassistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
